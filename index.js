070d62fb015193406fbc451a849baa34b851d666
BilRos <billyrossowuor@gmail.com>
2024-04-07 19:26:33 +0300

    Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.getElementById("text");
+    document.querySelector('p');
+    text.innerHTML="This is really cool!"
+  })
+
+  
